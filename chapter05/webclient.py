f3c565df07b694b95e1166c2e0f536cc03752a94
yfrenkel <yossi.frenkel@icloud.com>
2025-04-20 21:39:54 +1000

    add example HTTP requests and responses

--- chapter05/webclient.py
+++ chapter05/webclient.py
@@ -8,8 +8,9 @@ import argparse
 
 from pathlib import Path
 
+DEFAULT_HTTP_PORT = 80
 HTTP_ENCODING = 'ISO-8859-1'
-BUFF_SIZE = 4096
+RESPONSE_BUFFER_SIZE = 4096
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('webclient')
@@ -18,7 +19,7 @@ parser = argparse.ArgumentParser(
     description='Creates a simple web client in python using the socket library.'
 )
 parser.add_argument('host')
-parser.add_argument('port', nargs='?', default=80, type=int)
+parser.add_argument('port', nargs='?', default=DEFAULT_HTTP_PORT, type=int)
 args = parser.parse_args()
 logger.info(args)
 
@@ -27,7 +28,7 @@ s.connect((args.host, args.port))
 logger.info(f'Created new socket for {args.host=} at {args.port=}')
 
 example_http_get_request: bytes = (
-    Path('http_get_request')
+    Path('test/http_request')
     .read_text(newline='\r\n')
     .format(args.host)
     .encode(HTTP_ENCODING)
@@ -37,11 +38,11 @@ logger.info(f'Sent {example_http_get_request=} to {args.host=}')
 
 response_buffer = b''
 try:
-    d: bytes = s.recv(BUFF_SIZE)
+    d: bytes = s.recv(RESPONSE_BUFFER_SIZE)
     while len(d):
         logger.debug(f'Received (partial) response {d=}')
         response_buffer += d
-        d = s.recv(BUFF_SIZE)
+        d = s.recv(RESPONSE_BUFFER_SIZE)
     print(response_buffer.decode(HTTP_ENCODING))
 except socket.timeout:
     logger.warning('Timeout! No data received!')
